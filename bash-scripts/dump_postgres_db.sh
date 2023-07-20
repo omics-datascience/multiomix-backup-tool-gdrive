753ef6cd75b75da0b9383d15b7a86cb8a83d9057
Julián Muhlberger <muhlber@gmail.com>
2023-07-19 21:22:32 -0300

    New structure and main bash scripts

---
+++ bash-scripts/dump_postgres_db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#   This command will create a dump file of an postgres data base and will save inside
+#   a folder called like the db inside BACKUP_DIR environment variable.
+#
+#   Usage:
+#       ./dump_postgres_db.sh <host> <database-name> <username-env-var-name> <password-env-var-name>
+#       
+#   Credentials will be taken from Environment variables name in the arguments.
+#
+
+if [ $# -lt 4 ]
+then
+    echo "Error: Missing arguments." 1>/dev/stderr
+    echo -e "\nUsage:"
+    echo -e "    ./dump_postgres_db.sh <host> <database-name> <user-env-var-name> <pass-env-var-name>\n"
+    exit 1
+fi
+
+HOST=$1
+DATABASE=$2
+USERNAME=$3
+PASSWORD=$4
+START_DATE=$(date +%Y-%m-%d_%H-%M-%S_)
+
+mkdir -p $BACKUP_DIR/pg_dump/$START_DATE
+
+PGPASSWORD=$PASSWORD pg_dump --host=$HOST --dbname=$DATABASE --username=$USERNAME --data-only --no-owner | gzip > $BACKUP_DIR/pg_dump/$START_DATE$OUTPUT_FILENAME.sql.gz
